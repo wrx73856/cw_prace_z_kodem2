cb61048495d572fd4765d440092618d5df6df07b
wrx73856 <wrx73856@student.wsb.wroclaw.pl>
2022-04-14 20:35:54 +0200

    add docker

--- app.py
+++ app.py
@@ -1,28 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
-
-
+from flask import Flask
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Welcome', username='Mikolaj')
-
-
-@app.route('/index/<name>')
-def hello(name):
-    return render_template('index.html', title='Welcome', username=name)
-
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        user = request.form['name']
-        return redirect(url_for('hello', name=user))
-    else:
-        user = request.args.get('name')
-        return render_template('login.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+	return '<h1>Hello WSB! Greetings from Flask & Docker!</h1>'
+	
+if __name__ == "__main__":
+	app.run(debug=True)
